21b4f85c8734c117d1ea5057b78f3a4bb35ad607
abhijit <abhijit.guist@gmail.com>
2022-03-08 12:32:28 +0530

    Committed check symmetry of a binary tree

---
+++ Java/Tree/CheckSymmetryBinaryTree.java
@@ -0,0 +1,22 @@
+package Tree;
+
+import Tree.utility.CreateBinaryTree;
+import Tree.utility.TreeNode;
+
+public class CheckSymmetryBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = CreateBinaryTree.CreateBinaryTree();
+
+        System.out.printf("Is Symmetric: " + isSymmetric(root.left, root.right));
+    }
+
+    static boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null)
+            return left == right;
+
+        if (left.val != right.val)
+            return false;
+
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
