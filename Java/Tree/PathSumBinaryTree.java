e00508fb2774284bd00e4f826dd519508ee2ad8f
abhijit <abhijit.guist@gmail.com>
2022-03-08 13:03:58 +0530

    Committed find root to leaf path sum exists

---
+++ Java/Tree/PathSumBinaryTree.java
@@ -0,0 +1,32 @@
+package Tree;
+
+import Tree.utility.CreateBinaryTree;
+import Tree.utility.TreeNode;
+
+public class PathSumBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = CreateBinaryTree.CreateBinaryTree();
+        int targetSum = 10;
+        int currentSum = Integer.MIN_VALUE;
+
+        System.out.printf("Exist target sum: " + checkTargetSum(root, targetSum, currentSum));
+    }
+
+    static boolean checkTargetSum(TreeNode root, int targetSum, int currentSum) {
+        if (currentSum == Integer.MIN_VALUE)
+            currentSum = root.val;
+        else
+            currentSum += root.val;
+
+        if (targetSum == currentSum && root.left == null && root.right == null)
+            return true;
+
+
+        if (root.left != null && checkTargetSum(root.left, targetSum, currentSum) == true)
+            return true;
+        if (root.right != null && checkTargetSum(root.right, targetSum, currentSum) == true)
+            return true;
+
+        return false;
+    }
+}
